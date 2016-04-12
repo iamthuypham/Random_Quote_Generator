a4ddb4c7e47a62a6d4c4b9c438efa7589962edf8
Thuy Pham <tpham045@hotmail.com>
2016-04-11 20:41:05 -0700

    add comments and some starting code lines

--- random-quote-v1/js/script.js
+++ random-quote-v1/js/script.js
@@ -2,3 +2,19 @@
 // when user clicks anywhere on the page, the "makeQuote" function is called
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
+
+//Create an array of JavaScript objects to hold the data for your quotes.
+
+//Each quote object should have the following properties:
+var quote
+var source
+var citation
+var year
+
+//Create a function named getRandomQuote which:
+function getRandomQuote
+randomNum = round.ceil(Math.random() * maxNum) + 1
+
+
+//Create a function named printQuote which follows these rules:
+function printQuote
